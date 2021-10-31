e19330022a6609fe4c19d277e8cb97cdd723d99c
Rexhep Krasniqi <rkrasniqi93@gmail.com>
2021-10-31 19:30:41 +0100

    removed scantum routes & moved midleware from controller to api routes

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth')->prefix('auth')->group(function () {
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('login', 'App\Http\Controllers\AuthController@login')->withoutMiddleware('auth');
     Route::post('logout', 'App\Http\Controllers\AuthController@logout');
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
